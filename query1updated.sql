d7373763507876e5301c4a4028071f8aca938e8e
zaeemiqbalgoicon <152313512+zaeemiqbalgoicon@users.noreply.github.com>
2023-12-12 14:11:02 +0500

    Update query1updated.sql

--- query1updated.sql
+++ query1updated.sql
@@ -1,3 +1,4 @@
+select * from (
 select 
 	series.id as sId,
 	a.fk_parent_event_id as pid,
@@ -65,3 +66,4 @@ AND (a.is_rec = 0 or (a.is_rec = 1 and (activity_instances.fk_activity_id is not
 
 # Facility Filter
  AND a.fk_facility_id IN (1905)
+) s where s.occurrenceTime>=@startdate
