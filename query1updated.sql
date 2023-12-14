4b80a522988c17f1eef52d75286a3d2f5621cf0f
zaeemiqbalgoicon <152313512+zaeemiqbalgoicon@users.noreply.github.com>
2023-12-14 20:16:34 +0500

    Update query1updated.sql

--- query1updated.sql
+++ query1updated.sql
@@ -65,7 +65,7 @@ AND (a.is_rec = 0 or (a.is_rec = 1 and (activity_instances.fk_activity_id is not
 
 
 # Facility Filter
- AND a.fk_facility_id IN (1905)
+ AND a.fk_facility_id NOT IN (1905)
 ) s 
 where s.occurrenceTime >= @startdate
 AND s.Attendee is not null
